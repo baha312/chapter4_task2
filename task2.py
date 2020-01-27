1f951717822c5ccbc7a6b1b704325cb4f29401bb
baha312 <bahtiyarbolotbekov@gmail.com>
2020-01-27 10:10:25 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,44 @@
+# Создайте новый класс Airplane. Создайте следующие характеристики (полей)
+# объекта:
+# ● make (марка)
+# ● model
+# ● year
+# ● max_speed
+# ● odometer
+# ● is_flying
+# и методы имитирующих поведение самолета take off (взлет), fly (летать), land
+# приземление). Метод take off должен изменить is_flying на True, а land на False. По
+# умолчанию поле is_flying = False. Метод fly должен принимать расстояние полета и
+# изменять показания одометра (километраж). Создайте 1 объект класса и используйте
+# все методы класса.
+
+class Airplane():
+
+    def __init__(self, mark, model, year, max_speed):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+        self.odometer = 0
+        self.is_flying = False
+    
+    def take_off(self):
+        self.is_flying = True
+        message_take = f"{self.mark} {self.model} was take off."
+        return message_take
+
+    def fly(self, km):
+        self.odometer += km
+        message_fly = f"{self.mark} flew {self.odometer} km during the flying {self.max_speed} km/h."
+        return message_fly
+    
+    def land(self):
+        self.is_flying = False
+        message_land = f'{self.mark} landed, the odometer shows {self.odometer} km.'
+        return message_land
+    
+start = Airplane("Sukhoi", "Su-27", "2018", 2500)
+print(start.take_off())
+print(start.fly(600))
+print(start.fly(600))
+print(start.land())
